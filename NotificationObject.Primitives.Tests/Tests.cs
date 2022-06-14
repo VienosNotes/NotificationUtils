550741c6842d197c42d6fb7b4159ab6c5427e334
VienosNotes <vienosnotes@gmail.com>
2022-06-14 21:45:44 +0900

    initial commit

---
+++ NotificationObject.Primitives.Tests/Tests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace NotificationUtils.Primitives.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            string propName = null;
+            string newValue = null;
+            
+            var testObj = new DerivedClass()
+            {
+                Name = "ユウカ"
+            };
+
+            testObj.PropertyChanged += (o, e) =>
+            {
+                propName = e.PropertyName;
+                newValue = ((DerivedClass)o).Name;
+            };
+            
+            testObj.SetProperty(nameof(DerivedClass.Name), "ミドリ");
+            
+            Assert.AreEqual("ミドリ", testObj.Name, "対象のプロパティの値が変更されていること");
+            Assert.AreEqual("Name", propName, "変更通知イベントが受け取った引数のプロパティ名が正しいこと");
+            Assert.AreEqual("ミドリ", newValue, "変更通知イベントハンドラの中で対象の値が変更されていること");
+        }
+    }
+    
+    public class DerivedClass : NotificationObjectBase
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ NotificationUtils.Primitives/NotificationObjectBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace NotificationUtils.Primitives
+ {
+     /// <summary>
+     /// 変更通知オブジェクトの基本的な機能を提供します。
+     /// </summary>
+     public class NotificationObjectBase : INotifyPropertyChanged
+     {
+         /// <inheritdoc/>>
+         public event PropertyChangedEventHandler PropertyChanged;
+         
+         /// <summary>
+         /// プロパティへのセットを行う動的コードのキャッシュ。
+         /// </summary>
+         private static readonly Dictionary<DynamicPropertySetKey, Action<object, object>> InvokerCache = new Dictionary<DynamicPropertySetKey, Action<object, object>>();
+         
+         /// <summary>
+         /// 対象のプロパティに値を設定し、PropertyChanged イベントを発火します。
+         /// </summary>
+         /// <param name="propName">対象のプロパティ名。</param>
+         /// <param name="value">設定する値。</param>
+         /// <typeparam name="T">対象のプロパティの型。</typeparam>
+         public virtual void SetProperty<T>(string propName, T value)
+         {
+             var key = new DynamicPropertySetKey
+             {
+                 Type = GetType(), // 実行時の派生先の型
+                 Property = propName
+             };
+
+             // キャッシュに生成済みの動的コードがあれば使用し、なければ作ってキャッシュに追加する
+             if (!InvokerCache.TryGetValue(key, out var invoker))
+             {
+                 // セットされる新しい値
+                 var p = Expression.Parameter(typeof(object));
+                 // 対象のプロパティのオーナー
+                 var self = Expression.Parameter(typeof(object));
+                 
+                 var exp = Expression.Lambda<Action<object, object>>(
+                     Expression.Block(
+                         Expression.Assign(
+                             Expression.Property(
+                                 Expression.Convert(self, key.Type), 
+                                 propName),
+                             Expression.Convert(p, typeof(T))
+                             )
+                         ), self, p);
+                 invoker = exp.Compile();
+                 InvokerCache[key] = invoker;
+             }
+             
+             invoker.Invoke(this, value);
+             RaisePropertyChanged(propName);
+         }
+         
+         /// <summary>
+         /// PropertyChanged イベントを発火させます。 
+         /// </summary>
+         /// <param name="propName">プロパティ名。</param>
+         protected virtual void RaisePropertyChanged(string propName)
+         {
+             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+         }
+         
+         /// <summary>
+         /// プロパティセットを行う動的コードを区別するためのキー。
+         /// </summary>
+         private class DynamicPropertySetKey
+         {
+             /// <summary>
+             /// プロパティのオーナーの型。
+             /// </summary>
+             public Type Type;
+             /// <summary>
+             /// プロパティ名。
+             /// </summary>
+             public string Property;
+             
+             /// <inheritdoc/>
+             public override int GetHashCode()
+             {
+                 return Type.GetHashCode() ^ Property.GetHashCode();
+             }
+         }
+     }
+ }
